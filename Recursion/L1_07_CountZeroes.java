67d046a86b1d3c4569166b8cac09916fc3e56449
Kartik Agarwal <agarwalkartik453@gmail.com>
2025-02-21 16:36:50 +0530

    Create L1_07_CountZeroes.java

---
+++ Recursion/L1_07_CountZeroes.java
@@ -0,0 +1,20 @@
+// Count the number of zeroes in a number .
+
+public class L1_07_CountZeroes {
+    public static void main(String[] args) {
+        System.out.println(count(1004002202));
+    }
+    static int count(int n){
+        return helper(n,0);
+    }
+    static int helper(int n , int c){
+        if(n==0){
+            return c;
+        }
+        int rem = n%10;
+        if (rem == 0) {
+            return helper(n/10,c+1);
+        }
+        return helper(n/10,c);
+    }
+}

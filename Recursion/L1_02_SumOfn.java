21c809fc7337185d3254dc602055b83dd6f6b61f
Kartik Agarwal <agarwalkartik453@gmail.com>
2025-02-21 15:11:19 +0530

    Update L1_02_SumOfn.java

--- Recursion/L1_02_SumOfn.java
+++ Recursion/L1_02_SumOfn.java
@@ -3,8 +3,8 @@ public class L1_02_SumOfn {
         System.out.println(sum(6));
     }
     static int sum(int n){
-        if(n==1){
-            return 1;
+        if(n==0){
+            return 0;
         }
         return n+sum(n-1);
     }

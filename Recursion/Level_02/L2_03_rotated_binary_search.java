16e71e220baa0c9cae09c04e5e52970bf55f8421
Kartik Agarwal <agarwalkartik453@gmail.com>
2025-03-13 20:50:22 +0530

    Update L2_03_rotated_binary_search.java

--- Recursion/Level_02/L2_03_rotated_binary_search.java
+++ Recursion/Level_02/L2_03_rotated_binary_search.java
@@ -1,4 +1,4 @@
-public class L2_02_rotated_binary_search {
+public class L2_03_rotated_binary_search {
     public static void main(String[] args) {
         int[] arr = {5,6,7,8,9,1,2,3};
         int target = 2;

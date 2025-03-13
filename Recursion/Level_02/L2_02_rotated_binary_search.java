30e9b8708dfb2494bd0b88b1d82c7878a7620bb5
Kartik Agarwal <agarwalkartik453@gmail.com>
2025-03-13 20:49:12 +0530

    Create L2_02_rotated_binary_search.java

---
+++ Recursion/Level_02/L2_02_rotated_binary_search.java
@@ -0,0 +1,32 @@
+public class L2_02_rotated_binary_search {
+    public static void main(String[] args) {
+        int[] arr = {5,6,7,8,9,1,2,3};
+        int target = 2;
+        int ans = search(arr,target,0, arr.length-1);
+        System.out.println(ans);
+    }
+    static int search(int[] arr,int target,int s, int e){
+        if(s>e){
+            return -1;
+        }
+        int m = s+(e-s)/2;
+        if(arr[m]==target){
+            return m;
+        }
+        if(arr[s]<=arr[m]){
+            if(target>=arr[s] && target<=arr[m] ){
+                return search(arr,target,s,m-1);
+            }
+            else{
+                return search(arr,target,m+1,e);
+            }
+
+        }
+        if(target>=arr[m] && target<=arr[e]){
+            return search(arr,target,m+1,e);
+        }
+        else {
+            return search(arr,target,s,m-1);
+        }
+    }
+}
